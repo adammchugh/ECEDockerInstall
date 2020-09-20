1420969b111929ca110c1a370bd29d390b7a32a3
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-09-20 16:18:03 +0930

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -9,3 +9,5 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+sudo sysctl -w vm.max_map_count=262144
+sudo sysctl -p
