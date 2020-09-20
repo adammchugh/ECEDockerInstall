d09b1e062f6a521f1c60594d9ab21e81a3e927ab
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-09-20 17:05:34 +0930

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -13,4 +13,5 @@ sudo sysctl -w vm.max_map_count=262144
 sudo sysctl -p
 sudo mkdir /mnt/data
 sudo chown -R 1000:1000 /mnt/data/
-bash <(curl -fsSL https://download.elastic.co/cloud/elastic-cloud-enterprise.sh) install
+
+echo "bash <(curl -fsSL https://download.elastic.co/cloud/elastic-cloud-enterprise.sh) install --coordinator-host <hostname of first host> --roles-token 'TOKEN'"
