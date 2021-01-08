4b577891ebc1158beab45b9d5a1d0a1f8451d37d
adammchugh <34958726+adammchugh@users.noreply.github.com>
2021-01-08 19:51:47 +1030

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt-get install curl apt-transport-https ca-certificates software-properties-common -y
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update -y
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo systemctl status docker
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
-sudo usermod -aG docker $USER
+
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
 echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -w vm.max_map_count=262144
+
 sudo mkdir /mnt/data
 sudo chown 1000:1000 /mnt/data/
 
