652a7c5964eb4fb1a7262b64c4bdcecd6c66a5cd
Adam McHugh <34958726+adammchugh@users.noreply.github.com>
2021-06-05 11:13:23 +0930

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -8,7 +8,6 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
